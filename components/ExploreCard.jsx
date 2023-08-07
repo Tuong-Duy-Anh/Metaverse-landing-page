8192ba4882930b4ed3389e959994a52f2fab4042
Tuong-Duy-Anh <tdanh.ump@gmail.com>
2023-08-07 23:12:25 +0700

    finish world feature

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -23,7 +23,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-4 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+      <div className="absolute bottom-0 p-[8px] justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
